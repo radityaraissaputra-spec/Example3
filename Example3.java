daf6f04e04d93bd4928b7215035839da56d7ab46
radityaraissaputra-spec <radityaraissaputra@gmail.com>
2026-04-22 08:21:54 +0700

    Add files via upload

---
+++ Example3.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.example3;
+
+/**
+ *
+ * @author radit
+ */
+import java.util.Scanner;
+public class Example3
+{
+    public static void main (String[] args)
+    {
+        int nilai1;
+        System.out.println("program mulai ");
+        /*        System.out.println("inputkan Nilai" : );
+        if (nilai1 > 0)
+        System.out.println("bilangan positif");
+        else if (nilai < 0)
+        System.out.println("bilangan nol");
+        System.out.println("program selesai");*/           
+    }
+   
+}
